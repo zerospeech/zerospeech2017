587a34afc4af33b0d82d4d47608ade99a3da73d7
Juan Benjumea <jubenjum@gmail.com>
2017-01-16 22:14:15 +0100

    correction on config, data list and scripts on baseline_french

--- track2/baseline/baseline_french/run.sh
+++ track2/baseline/baseline_french/run.sh
@@ -5,6 +5,13 @@ set -e
 # including the input variables from the file config
 . config
 
+
+### creating lsh and feature files in data/cache
+cd data
+./run.sh
+cd -
+
+
 ### run discovery for parameters on the 'config' file
 ./run_disc   
 
